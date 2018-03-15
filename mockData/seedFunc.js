8a260e9d86e7f7dd9f4f858aa4c1686538f22ea6
Arjun Chemparathy <arjunchemparathy@gmail.com>
2018-03-15 11:17:01 -0700

    used createWriteStream to seed 1 million records in 55 seconds

--- mockData/seedFunc.js
+++ mockData/seedFunc.js
@@ -1,10 +1,8 @@
 Promise = require("bluebird");
-
 const faker = require('faker');
 const mongoose = require('mongoose');
-// mongoose.Promise = require('bluebird');
+const fs = require('fs');
 
-// Promise.promisifyAll(require("mongodb"));
 let dbURI = 'mongodb://localhost/ReviewTest';
 
 const reviewSchema = mongoose.Schema({
@@ -43,35 +41,148 @@ const fakeReview = () => {
     reviewObj.funny = faker.random.number();
     reviewObj.cool = faker.random.number();
 
-    return reviewObj;
+    return JSON.stringify(reviewObj);
 };
 
-const addtoDB = (num) => {
-  let start = new Date()
-  mongoose.connect(dbURI);
-  // let db = mongoose.connection;
-  // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  let counter = 0;
-  for (var i = 0; i < num; i++) {
-    const newReview = fakeReview();
-    const review = new Review(newReview);
-    review.save().then(()=> {
-      counter++;
-      if (counter === num) {
-        let end = new Date();
-        console.log(`Inserted ${num} in ${ (end - start)/1000 } seconds`);
-        mongoose.connection.close();
+// Write the data to the supplied writable stream one million times.
+// Be attentive to back-pressure.
+// function writeOneMillionTimes(writer, data, encoding, callback) {
+let wstream = fs.createWriteStream('myOutput.txt');
+function writeOneMillionTimes(writer, encoding, callback) {
+  let i = 1000000;
+  write();
+  // for (let j = 0; i < records; i++) {
+  //   let review = fakeReview();
+  //   fs.appendFileSync(`./${fileNumber}.txt`, JSON.stringify(review));
+  // };
+  function write() {
+    let ok = true;
+    do {
+      i--;
+      if (i === 0) {
+        // last time!
+        writer.write(fakeReview() +'/n', encoding, callback);
+      } else {
+        // see if we should continue, or wait
+        // don't pass the callback, because we're not done yet.
+        ok = writer.write(fakeReview() +'/n', encoding);
       }
-    });
+    } while (i > 0 && ok);
+    if (i > 0) {
+      // had to stop early!
+      // write some more once it drains
+      writer.once('drain', write);
+    }
   }
-};
+}
+
+writeOneMillionTimes(wstream, 'utf8', () => console.log('done!'));
+
+// ----------------------------------2 mins to write 1 million records to file ---------------------------
+// let writeToFile = (num) => {
+//   // let start = new Date();
+//   let fileNumber = 1;
+  
+//   for (let i = 0; i < num; i++) {
+//     let review = fakeReview();
+//     fs.appendFileSync(`./${fileNumber}.txt`, JSON.stringify(review));
+//   };
+//   // let end = new Date();
+//   // console.log(`Inserted ${num} in ${ (end - start)/1000 } seconds`);
+// }
+
+// writeToFile(1000000);
+//--------------------------------------------------------------------------------------------------------------
+
+// ----------------------------------insert 10000 
+// const addtoDB = (num) => {
+//   mongoose.connect(dbURI);
+//   let counter = 1;
+//   let increment = 10000
+
+//   let write100 = (arr) => {
+//     let start = new Date();
+//     Review.insertMany(arr).then(()=>{
+//       if(counter <= num) {
+//         next100();
+//         let end = new Date();
+//         console.log(`Inserted ${increment} in ${ (end - start)/1000 } seconds`);
+//         // console.log("done")
+//       }
+//     });
+//     };
+
+//   let next100 = () => {
+//     let reviews = [];
+//     for (let j = counter; j <= (counter + increment); j++) {
+//       // console.log(j)
+//       // console.log(reviews.length);
+//       const newReview = fakeReview();
+//       reviews.push(newReview);
+//       if(counter <= num && reviews.length === increment) {
+//         // console.log("j", j, "counter", counter)
+//         counter += increment;
+//         // if ()
+//         write100(reviews);
+//       } 
+//     }
+//   }
+
+//   next100();
+// };
+
+// addtoDB(100000);
+
+//-------------------------------1000 in 0.07 seconds/ 500K in 40 secs------------------------------------------
+// const addtoDB = (num) => {
+//   let start = new Date()
+//   mongoose.connect(dbURI);
+//   let counter = 0;
+//   let arr = [];
+
+//   for (var i = 0; i < num; i++) {
+//     const newReview = fakeReview();
+//     arr.push(newReview);
+//   }
+
+//   Review.collection.insert(arr, () => {
+//     console.log("done!")
+//     let end = new Date();
+//     console.log(`Inserted ${num} in ${ (end - start)/1000 } seconds`);
+//     mongoose.connection.close();
+//   });
+// };
+
+// addtoDB(500000);
+//----------------------------------------------------------------------------------------------------------------
+
+//1000 in 1.333 seconds--------------------------------------------------------------------------------------------
+// const addtoDB = (num) => {
+//   let start = new Date()
+//   mongoose.connect(dbURI);
+//   // let db = mongoose.connection;
+//   // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//   let counter = 0;
+//   for (var i = 0; i < num; i++) {
+//     const newReview = fakeReview();
+//     const review = new Review(newReview);
+//     review.save().then(()=> {
+//       counter++;
+//       if (counter === num) {
+//         let end = new Date();
+//         console.log(`Inserted ${num} in ${ (end - start)/1000 } seconds`);
+//         mongoose.connection.close();
+//       }
+//     });
+//   }
+// };
 
 
-addtoDB(2000);
+// addtoDB(1000);
 // addtoDB(2000);
 // addtoDB(2000);
 // addtoDB(2000);
-
+//-----------------------------------------------------------------------------------------------------------------
 //................................................................works but ...
 // const addtoDB = new Promise((resolve, reject) => {
 
